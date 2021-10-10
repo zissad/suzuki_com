be3b282c6a91a6c5860ece562e7b818c781e8837
zissad <88398384+zissad@users.noreply.github.com>
2021-10-10 14:05:54 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var fr_el = document.querySelector('#fr_el');
+var nd_el = document.querySelector('#nd_el');
+
+var fr_el1 = document.querySelector('#fr_el1');
+var nd_el1 = document.querySelector('#nd_el1');
+
+function dt_1(){
+    fr_el.innerHTML = 'Engin(CC)';
+    nd_el.innerHTML = '147.3';
+    fr_el1.innerHTML = 'Prize(BDT)';
+    nd_el1.innerHTML = '395,000';
+}
+
+function dt_2(){
+    fr_el.innerHTML = 'POWER (PS)';
+    nd_el.innerHTML = '14.1';
+    fr_el1.innerHTML = 'WEIGHT (KG)';
+    nd_el1.innerHTML = '131';
+}
+
+function dt_3(){
+    fr_el.innerHTML = 'Ground Clearance (mm)';
+    nd_el.innerHTML = '160';
+    fr_el1.innerHTML = 'Fuel Tank Capacity (l)';
+    nd_el1.innerHTML = '11';
+}
